98b274423e6d3abb229e538670327ff37968360a
LeviUniAcc <otterbach.levi1@gmail.com>
2025-07-09 18:46:52 +0200

    initial frontend

---
+++ tanks-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
