610d443b996f32c82e7c921fd32025c701996088
LeviUniAcc <otterbach.levi1@gmail.com>
2025-07-10 20:36:48 +0200

    initial backend

---
+++ tanks-backend/tanks-backend/src/main/java/com/tanks/backend/tanks_backend/TanksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tanks.backend.tanks_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanksBackendApplication.class, args);
+	}
+
+}
---
+++ tanks-backend/tanks-backend/src/test/java/com/tanks/backend/tanks_backend/TanksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanks.backend.tanks_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
